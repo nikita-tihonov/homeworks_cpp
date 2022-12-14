77f92ca6c064a76168ec1c1c9630a144fd087508
NikitaTikhonov <tihonoff.nik@gmail.com>
2022-12-14 15:45:04 +0000

    wip

--- homework_01/task_01/src/utils.cpp
+++ homework_01/task_01/src/utils.cpp
@@ -3,5 +3,36 @@
 #include <stack>
 
 std::vector<std::string> SplitString(const std::string& data) {
-  return {};
+  std::vector<std::string>res;
+  std::string w = "";
+  int k = 0;
+  for (auto c : data) {
+    if (c == '(') {
+      k++;
+      w += c;
+      continue;
+    }
+    if (c == ')') {
+      k--;
+      w += c;
+      continue;
+    }
+    if (k == 0) {
+if ((c != '\t') && (c != ' ')) {
+        w += c;
+      } else {
+        if (!w.empty()) {
+          res.push_back(w);
+          w = "";
+        }
+      }
+    } else {
+      w += c;
+    }
+  }
+  if (!w.empty()) {
+    res.push_back(w);
+  }
+  return res;
+
 }
--- homework_01/task_01/src/utils.hpp
+++ homework_01/task_01/src/utils.hpp
@@ -3,5 +3,6 @@
 #include <string>
 #include <string_view>
 #include <vector>
+#include<utils.cpp>
 
 std::vector<std::string> SplitString(const std::string& data);
